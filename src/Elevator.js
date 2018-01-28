a14ce1a374a7dd2d16e29637c2fc25c1e106fe5e
Matt Kime <matt@mattki.me>
2018-01-28 10:43:00 -0600

    stub out elevator movement functionaity

--- src/Elevator.js
+++ src/Elevator.js
@@ -1,5 +1,18 @@
 module.exports = class Elevator {
     constructor(floorCount) {
         this.floorCount = floorCount;
+        this.currentFloor = 1;
+    }
+    validateFloor(floor) {
+        return floor >= 1 && floor <= this.floorCount;
+    }
+    goTo(targetFloor) {
+        if (!this.validateFloor(targetFloor)) {
+            throw Error("Invalid floor number");
+        }
+        // compare targetFloor to currentFloor
+        // go to next closest floor
+        // fire event on next floor
+        // repeat until at target floor
     }
 }
