d389514eb829e31dfe2af555a379b1401556b639
Matt Kime <matt@mattki.me>
2018-01-28 10:56:48 -0600

    track trips for service needs

--- src/Elevator.js
+++ src/Elevator.js
@@ -2,6 +2,8 @@ module.exports = class Elevator {
     constructor(floorCount) {
         this.floorCount = floorCount;
         this.currentFloor = 1;
+        this.tripsMade = 0;
+        this.tripsSinceService = 0;
     }
     validateFloor(floor) {
         return floor >= 1 && floor <= this.floorCount;
@@ -10,9 +12,29 @@ module.exports = class Elevator {
         if (!this.validateFloor(targetFloor)) {
             throw Error("Invalid floor number");
         }
+        if (!this.isInService()) {
+            throw Error("Elevator out of service");
+        }
+        if (this.currentFloor == targetFloor) { return; }
+        this.tripsMade++;
+        this.tripsSinceService++;
         // compare targetFloor to currentFloor
         // go to next closest floor
         // fire event on next floor
         // repeat until at target floor
+
+        //open doors
+    }
+    open() {
+        // report opening doors
+    }
+    close() {
+        //report closing doors
+    }
+    isInService() {
+        return this.tripsSinceService <= 100;
+    }
+    resetServiceInterval() {
+        this.tripsSinceService = 0;
     }
 }
